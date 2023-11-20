4f892e9f3d5c434cc0c463cd9ddb23e0f91f367d
JuanX_B <bertoia114@gmail.com>
2023-11-20 19:49:35 -0300

    create method with volt

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -2,12 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Response;
+use Illuminate\View\View;
 
 class ChirpController extends Controller
 {
-    public function index(): Response
+    public function index(): View
     {
-        return response("Hello World!");
+        return view('chirps', []);
     }
 }
